9443b1a619ef37587239ef4e6fb48f84af4b1a5f
Prabhjot Sidhu <psidhu08@linux.socs.uoguelph.ca>
2024-02-28 12:34:09 -0500

    encountering issues regarding types

--- cal.adb
+++ cal.adb
@@ -1,27 +1,67 @@
-with Ada.Text_IO; use Ada.Text_IO;
+with ada.Text_IO; use ada.Text_IO;
+with ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with ada.Float_Text_IO; use ada.Float_Text_IO;
+with ada.Strings.Fixed; use ada.Strings.Fixed;
 
-procedure Cal is
-   response: Boolean;
-   
-   function isValid(year: integer) return Boolean is
+procedure cal is
+
+   year: Integer;
+   firstDay: String(1..9);
+   lang: String(1..7);
+
+   type daysArray is array(0..6) of String(1..9);
+   days: constant daysArray := (
+      0 => "Monday"   & "   ",
+      1 => "Tuesday"  & "  ",
+      2 => "Wednesday",
+      3 => "Thursday" & " ",
+      4 => "Friday"   & "    ",
+      5 => "Saturday" & " ",
+      6 => "Sunday"   & "   "
+   ); 
+
+   function isYearValid(year: Integer) return Boolean is
    begin
-      -- check if the inputted year is valid
-      if year >= 1582 then
-         return True;
-      else
-         return False;
-      end if;
-   end isValid;
+      -- Check if the inputted year is valid
+      return year >= 1582;
+   end isYearValid;
 
-   procedure readCalInfo(year: in integer)
+   procedure readCalInfo(
+      year     : out Integer;
+      firstDay : out String;
+      lang     : out String
+   ) is
+      inputYear : Integer;
+      calcYear : Integer;
+      dayForFirstIndex: Integer;
+      validInputYear: Boolean := False;
+   begin
+      while not validInputYear loop
+         put_Line("Enter a year greater than 1582 (inclusive):");
+         get(inputYear);
+         put_line("");
+         
+         validInputYear := isYearValid(inputYear);
+
+         if not validInputYear then
+            put_Line("Please ensure that your entered year is a valid integer and greater than 1582 (inclusive).");
+            put_line("");
+         else
+            put_Line(trim(Integer'Image(inputYear), side => ada.strings.Left) & " is a valid year!");
+         end if;
+      end loop;
+
+      --calculate the day that will the the first of the month
+      calcYear := inputYear - 1;
+      dayForFirstIndex := (36 + calcYear + (calcYear / 4) - (calcYear / 100) + (calcYear / 400)) mod 7; 
+      firstDay := days(dayForFirstIndex);
+      
+      year := inputYear;  -- Assigning the valid year with :=
+      lang := "English";  -- Placeholder value with :=
+   end readCalInfo;
    
 begin
-   -- print out calendar
-   put_line("Hello world");
-   
-   -- call the IsValid function and assign the result to Response
-   response := IsValid(1582);
-   
-   -- print the result
-   put_line("Is valid: " & Boolean'Image(Response));
-end Cal;
+   -- Test readCalInfo
+   readCalInfo(year, firstDay, lang);
+   put_Line("Year: " & Integer'Image(year) & ", FirstDay: " & firstDay & ", Lang: " & lang);
+end cal;
